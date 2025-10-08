899a552ed2504207c176a3017a17ea55cb106bd3
ziadafifi27 <zmarwan456@gmail.com>
2025-10-08 12:17:45 +0300

    task 2

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -31,7 +31,19 @@ const loginSchema = Joi.object({
 
 // TODO: implement login function
 export async function login(req, res, next) {
- 
+  try{
+    const { value, error } = loginSchema.validate(req.body);
+    if (error) return res.status(400).json({ message: error.message });
+    const user =  await User.findOne({ email: value.email });
+    if(!user) return res.status(401).json({ message: 'Invalid email or password' });
+    const isPasswordValid = await bcrypt.compare(value.password, user.passwordHash);
+    if(!isPasswordValid) return res.status(401).json({ message: 'Invalid email or password' });
+    const token = signToken(user);
+    res.json({ token, user: publicUser(user), message: 'Login successful' });
+  }
+  catch(err){
+    res.status(500).json({ message: err.message });
+  }
 }
 
 export async function me(req, res) {
